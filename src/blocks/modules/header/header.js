5b6f9c43845cc6a022f8544fca4ef57066391ce4
Danya Shemet <shemetg@gmail.com>
2020-01-14 18:46:40 +0200

    header, first screen content

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,11 +2,12 @@ import $ from "jquery";
 import "velocity-animate/velocity.min"
 import "velocity-animate/velocity.ui.min"
 
-let burger = $('.burger-nav');
+let burger = $('.burger-icon');
 let links = $('nav ul li a');
 let menu = $('nav ul li');
 let overlay_navigation = $('.overlay-navigation');
 
+// Burger menu functionality
 burger.click(showMenu)
 links.click(hideMenu);
 
@@ -14,7 +15,7 @@ function showMenu() {
     burger.css('pointer-events', 'none');
     menu.css('flex-basis', `calc(100%/ ${menu.length})`)
     overlay_navigation.toggleClass('overlay-active');
-    if(overlay_navigation.hasClass('overlay-active')){
+    if (overlay_navigation.hasClass('overlay-active')) {
         burger.addClass('active');
         $("body").addClass("fixed");
         overlay_navigation.velocity('transition.slideLeftIn', {
@@ -28,12 +29,12 @@ function showMenu() {
                 })
             }
         })
-    }else{
+    } else {
         hideMenu()
     }
 }
 
-function hideMenu(){
+function hideMenu() {
     burger.css('pointer-events', 'none');
     burger.removeClass('active')
     overlay_navigation.removeClass('overlay-active');
@@ -50,3 +51,13 @@ function hideMenu(){
         }
     })
 }
+
+// On Scroll Functionality
+$(window).scroll(function (){
+    let st = $(this).scrollTop();
+    let windowTop = $(window).scrollTop();
+    windowTop > 100 ? $('.burger-wrapper').addClass('navShadow') : $('.burger-wrapper').removeClass('navShadow');
+    $('.header-wrapper .container').css({
+        "transform": "translate(0%, " + -st / 2 + "px"
+    });
+});
