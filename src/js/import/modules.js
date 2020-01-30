08b03c6d5b5fe6292d98df0ca1919eba502b8270
Danya Shemet <shemetg@gmail.com>
2020-01-30 18:09:56 +0200

    start development block of price

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -3,5 +3,7 @@ import "%modules%/about/about";
 import "%modules%/portfolio/portfolio";
 import "%modules%/team/team";
 import "%modules%/feedback/feedback";
+import "%modules%/price/price";
 import "%modules%/footer/footer";
+
 
