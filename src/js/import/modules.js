69d8b928423ff9758e79af739671427109950ed3
Daniil Shemet <shemetg@gmail.com>
2020-02-02 11:46:31 +0200

    iml

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,9 +1,7 @@
 import "%modules%/header/header";
 import "%modules%/about/about";
 import "%modules%/portfolio/portfolio";
-import "%modules%/team/team";
 import "%modules%/feedback/feedback";
-import "%modules%/price/price";
 import "%modules%/footer/footer";
 
 
