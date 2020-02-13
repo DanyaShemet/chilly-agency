dc1d5218b518756e3be7eb64e981abd115237960
Daniil Shemet <shemetg@gmail.com>
2020-02-14 01:40:39 +0200

    small fix

--- src/blocks/modules/feedback/feedback.js
+++ src/blocks/modules/feedback/feedback.js
@@ -5,11 +5,11 @@ import 'owl.carousel/dist/owl.carousel.min'
 // slider functionality
 $('.feedback__slider').owlCarousel({
     loop: true,
-    margin: 10,
+    margin: 5,
     responsive: {
         300: {
             items: 1
         }
     },
-    smartSpeed: 700
+    smartSpeed: 600
 });
