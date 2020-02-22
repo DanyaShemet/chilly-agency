5d9f087f4f5de38ce367f571ec83a59b9f0a2115
Daniil Shemet <shemetg@gmail.com>
2020-02-22 23:06:00 +0200

    fix menu, vh

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -4,6 +4,9 @@ import "velocity-animate/velocity.ui.min"
 import $ from "jquery";
 import ModalVideo from "modal-video/js/modal-video.min"
 
+// 100 vh in mobile phone
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 let burger = $('.burger-icon');
 let links = $('.header__animate-nav ul li a');
