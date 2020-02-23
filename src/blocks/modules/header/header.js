3c705bbb1d0bcf11c71647226ea4763723d730d4
Daniil Shemet <shemetg@gmail.com>
2020-02-23 22:11:59 +0200

    fix footer, feach vh

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -10,14 +10,7 @@ let burger = $('.burger-icon');
 let links = $('.header__animate-nav ul li a');
 let menu = $('.header__animate-nav ul li');
 let overlay_navigation = $('.header__navigation-overlay');
-// 100vh in mobile phone
-let vh = window.innerHeight * 0.01;
 
-export function mobileDisplayInBrowser(){
-    if (document.documentElement.clientWidth <= 500){
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    }
-}
 
 // Burger menu functionality
 burger.click(showMenu)
@@ -104,3 +97,8 @@ export function scrollto() {
 // Video functionality
 new ModalVideo('.open-video');
 
+// 100vh in mobile phone
+export function mobileDisplayInBrowser(){
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
