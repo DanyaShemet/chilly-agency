4e6fd86e0c6c26f91fe13d4336a92ce098a34214
Daniil Shemet <shemetg@gmail.com>
2020-02-26 23:34:02 +0200

    release 03 mp version

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -5,11 +5,10 @@ import $ from "jquery";
 import ModalVideo from "modal-video/js/modal-video.min"
 
 
-let burger = $('.burger-icon');
-let links = $('.header__animate-nav ul li a');
-let menu = $('.header__animate-nav ul li');
-let overlay_navigation = $('.header__navigation-overlay');
-
+const burger = $('.burger-icon');
+const links = $('.header__animate-nav ul li a');
+const menu = $('.header__animate-nav ul li');
+const overlay_navigation = $('.header__navigation-overlay');
 
 // Burger menu functionality
 burger.click(showMenu)
@@ -63,8 +62,9 @@ $(window).scroll(function (e) {
 
 // opening menu in main page
 document.onwheel = onWheel;
-function onWheel(){
-    if (document.body.clientHeight <= 1500){
+
+function onWheel() {
+    if (document.body.clientHeight <= 1500) {
         showMenu();
     }
 }
@@ -73,7 +73,7 @@ function onWheel(){
 new ModalVideo('.open-video');
 
 // 100 vh in mobile phone
-export function mobileDisplayInBrowser(){
+export function mobileDisplayInBrowser() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
